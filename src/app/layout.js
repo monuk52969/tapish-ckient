56e370bace3f16aec9bb3280017747f98d9b7280
Monu Kumar <monuk52069@gmail.com>
2025-08-31 23:36:09 +0530

    Update Footer with social icons and responsive design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavDemo from "./components/ui/NavDemo";
 import Script from "next/script"; 
 import WhatsAppButton from "@/app/components/custombtns/WhatsAppButton ";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -30,9 +19,7 @@ export default function RootLayout({ children }) {
           strategy="beforeInteractive"
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {/* Navbar */}
         <NavDemo />
 
